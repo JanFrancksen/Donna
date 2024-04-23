865142840b9ad2c90ca3fae50e2be2d44315cd88
Jan Francksen <jfr@cleverreach.com>
2024-04-23 19:51:35 +0200

    feat: add homepage and biome linting for tailwind classes

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,10 @@
-'use client';
-import { Button } from '@repo/ui/Button';
-
 export default function Page(): JSX.Element {
 	return (
-		<div>
-			<Button variant='primary'>Hallo</Button>
+		<div className='text-center'>
+			<h1 className='bg-gradient-to-r from-red-500 to-purple-500 bg-clip-text font-bold text-3xl text-transparent'>
+				Hello, Belladonna!
+			</h1>
+			<p className='text-white'>You can do the thing! Believe in yourself.</p>
 		</div>
 	);
 }

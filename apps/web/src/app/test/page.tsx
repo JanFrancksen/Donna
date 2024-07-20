02823e3cd68150951bfdef8aeed7551e0faf924d
Jan Francksen <jfr@cleverreach.com>
2024-07-20 14:16:27 +0200

    feat: working breadcrumb

--- apps/web/src/app/test/page.tsx
+++ apps/web/src/app/test/page.tsx
@@ -1,19 +1,13 @@
 'use client';
-import { AlertDialog } from '@repo/ui/AlertDialog/AlertDialog';
+import { Breadcrumb, Breadcrumbs } from '@repo/ui/Breadcrumb/Breadcrumbs';
 
 function TestPage() {
 	return (
 		<div className='text-white'>
-			<AlertDialog
-				title='Hallo'
-				actionLabel='Test'
-				onAction={() => alert('test')}
-			>
-				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit
-				perspiciatis sit ad animi, nesciunt repellendus magni ab mollitia.
-				Quibusdam repudiandae rem eligendi dolorem enim adipisci temporibus,
-				magnam odio sequi rerum.
-			</AlertDialog>
+			<Breadcrumbs>
+				<Breadcrumb href='/'>Test</Breadcrumb>
+				<Breadcrumb>Test</Breadcrumb>
+			</Breadcrumbs>
 		</div>
 	);
 }
--- packages/ui/src/Breadcrumb/Breadcrumbs.tsx
+++ packages/ui/src/Breadcrumb/Breadcrumbs.tsx
@@ -2,32 +2,24 @@ import { ChevronRight } from 'lucide-react';
 import {
 	Breadcrumb as AriaBreadcrumb,
 	Breadcrumbs as AriaBreadcrumbs,
+	Link as AriaLink,
 	type BreadcrumbProps,
 	type BreadcrumbsProps,
 	type LinkProps,
 } from 'react-aria-components';
-import { twMerge } from 'tailwind-merge';
-import { Link } from '../Link/Link';
 
 export function Breadcrumbs<T extends object>(props: BreadcrumbsProps<T>) {
-	return (
-		<AriaBreadcrumbs
-			{...props}
-			className={twMerge('flex gap-1', props.className)}
-		/>
-	);
+	return <AriaBreadcrumbs {...props} className='flex items-center gap-x-1' />;
 }
 
 export function Breadcrumb(props: BreadcrumbProps & LinkProps) {
 	return (
-		<AriaBreadcrumb
-			{...props}
-			className={twMerge('flex items-center gap-1', props.className)}
-		>
-			<Link variant='secondary' {...props} />
-			{props.href && (
-				<ChevronRight className='h-3 w-3 text-gray-600 dark:text-zinc-400' />
-			)}
+		<AriaBreadcrumb {...props} className='flex items-center gap-x-1'>
+			<AriaLink
+				className='max-w-40 truncate rounded text-xs transition-colors hover:text-body-weak focus:outline-none focus-visible:ring-2 focus-visible:ring-cyan-500 focus-visible:ring-offset-4 aria-[current]:text-accent'
+				{...props}
+			/>
+			{props.href && <ChevronRight className='size-5 text-body-weak' />}
 		</AriaBreadcrumb>
 	);
 }

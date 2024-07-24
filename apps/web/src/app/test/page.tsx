5e169afacf92119439ac41f569122a345785dae0
Jan Francksen <jfr@cleverreach.com>
2024-07-24 17:50:20 +0200

    feat: add Datefield styling

--- apps/web/src/app/test/page.tsx
+++ apps/web/src/app/test/page.tsx
@@ -1,16 +1,10 @@
 'use client';
-import { ComboBox, ComboBoxItem } from '@repo/ui/ComboBox/ComboBox';
+import { DateField } from '@repo/ui/DateField/DateField';
 
 function TestPage() {
 	return (
 		<div className='text-white'>
-			<ComboBox>
-				<ComboBoxItem>Test</ComboBoxItem>
-				<ComboBoxItem>Test</ComboBoxItem>
-				<ComboBoxItem>Test</ComboBoxItem>
-				<ComboBoxItem>Rahdan</ComboBoxItem>
-				<ComboBoxItem>test 3</ComboBoxItem>
-			</ComboBox>
+			<DateField isDisabled />
 		</div>
 	);
 }
--- packages/ui/src/DateField/DateField.tsx
+++ packages/ui/src/DateField/DateField.tsx
@@ -1,4 +1,3 @@
-import { cva } from 'class-variance-authority';
 import {
 	DateField as AriaDateField,
 	type DateFieldProps as AriaDateFieldProps,
@@ -8,13 +7,7 @@ import {
 	type DateValue,
 	type ValidationResult,
 } from 'react-aria-components';
-import {
-	Description,
-	FieldError,
-	Label,
-	fieldGroupStyles,
-} from '../Field/Field';
-import { composeTailwindRenderProps } from '../examples/utils';
+import { Description, FieldError, Label } from '../Field/Field';
 
 export interface DateFieldProps<T extends DateValue>
 	extends AriaDateFieldProps<T> {
@@ -30,13 +23,7 @@ export function DateField<T extends DateValue>({
 	...props
 }: DateFieldProps<T>) {
 	return (
-		<AriaDateField
-			{...props}
-			className={composeTailwindRenderProps(
-				props.className,
-				'flex flex-col gap-1'
-			)}
-		>
+		<AriaDateField {...props} className='flex flex-col gap-1'>
 			{label && <Label>{label}</Label>}
 			<DateInput />
 			{description && <Description>{description}</Description>}
@@ -45,35 +32,18 @@ export function DateField<T extends DateValue>({
 	);
 }
 
-const segmentStyles = cva(
-	'inline rounded p-0.5 type-literal:px-0 text-gray-800 caret-transparent outline outline-0 forced-color-adjust-none dark:text-zinc-200 forced-colors:text-[ButtonText]',
-	{
-		variants: {
-			isPlaceholder: {
-				true: 'text-gray-600 italic dark:text-zinc-400',
-			},
-			isDisabled: {
-				true: 'text-gray-200 dark:text-zinc-600 forced-colors:text-[GrayText]',
-			},
-			isFocused: {
-				true: 'bg-blue-600 text-white dark:text-white forced-colors:bg-[Highlight] forced-colors:text-[HighlightText]',
-			},
-		},
-	}
-);
-
 export function DateInput(props: Omit<DateInputProps, 'children'>) {
 	return (
 		<AriaDateInput
-			className={(renderProps) =>
-				fieldGroupStyles({
-					...renderProps,
-					class: 'block min-w-[150px] px-2 py-1.5 text-sm',
-				})
-			}
+			className='group flex h-9 min-w-32 items-center overflow-hidden rounded-lg border-2 bg-background-weak px-3 py-2 text-sm disabled:border-stroke-weak forced-colors:bg-[Field]'
 			{...props}
 		>
-			{(segment) => <DateSegment segment={segment} className={segmentStyles} />}
+			{(segment) => (
+				<DateSegment
+					segment={segment}
+					className='rounded p-0.5 type-literal:px-0 text-text caret-transparent outline outline-0 forced-color-adjust-none focus:bg-brand disabled:text-stroke forced-colors:text-[ButtonText] disabled:forced-colors:text-[GrayText] focus:forced-colors:bg-[Highlight] focus:forced-colors:text-[HighlightText]'
+				/>
+			)}
 		</AriaDateInput>
 	);
 }

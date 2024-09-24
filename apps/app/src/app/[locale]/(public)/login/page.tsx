55137bee284427568217bbeef3700ff3efc3bc1e
Jan Francksen <jan.francksen@gmail.com>
2024-09-24 23:16:27 +0200

    feat: add ci

--- apps/app/src/app/[locale]/(public)/login/page.tsx
+++ apps/app/src/app/[locale]/(public)/login/page.tsx
@@ -9,13 +9,15 @@ export default function Page() {
   return (
     <div className='flex h-screen w-screen flex-col items-center justify-center'>
       <div className='flex size-96 flex-col items-center justify-center'>
-        <Image
-          src='/logo.png'
-          alt='logo'
-          className='mb-8 rounded-full border border-gray-800'
-          width={100}
-          height={100}
-        />
+        <a href='/'>
+          <Image
+            src='/logo.png'
+            alt='logo'
+            className='mb-8 rounded-full border border-gray-800'
+            width={100}
+            height={100}
+          />
+        </a>
         <h1 className='mb-2 font-bold text-2xl'>Welcome to Donna</h1>
         <p className='mb-8 text-gray-500'>Sign in with Github to continue</p>
         <GithubSignin />

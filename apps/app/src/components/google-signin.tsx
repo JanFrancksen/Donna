ca576a45352ebbb33be6135ee119396ab5fbfa63
Jan Francksen <jan.francksen@gmail.com>
2024-09-24 20:26:44 +0200

    feat: add some stuff

--- apps/app/src/components/google-signin.tsx
+++ apps/app/src/components/google-signin.tsx
@@ -8,7 +8,7 @@ export function GoogleSignin() {
 
   const handleSignin = () => {
     supabase.auth.signInWithOAuth({
-      provider: 'google',
+      provider: 'github',
       options: {
         redirectTo: `${window.location.origin}/api/auth/callback`,
       },
@@ -17,7 +17,7 @@ export function GoogleSignin() {
 
   return (
     <Button onClick={handleSignin} variant='outline' className='font-mono'>
-      Sign in with Google
+      Sign in with Github
     </Button>
   );
 }

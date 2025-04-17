f83bcd1adbace156e0d285fefade7d9fc3290d3b
Jan Francksen <jan.francksen@gmail.com>
2025-04-17 23:12:07 +0200

    feat: add signin logic again

--- apps/web/app/(auth)/layout.tsx
+++ apps/web/app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className='flex h-screen w-screen flex-col items-center justify-center'>
+    <div className='flex h-screen w-screen flex-col items-center justify-center gap-12'>
       <Image src='/onyxsoft_logo.png' alt='logo' width={260} height={38} />
       {children}
     </div>

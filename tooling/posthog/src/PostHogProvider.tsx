b8e82c4346b3b1173ff3cfb9c904616986cc2b1f
Jan Francksen <jan.francksen@gmail.com>
2025-04-22 21:54:16 +0200

    chore: update deps

--- tooling/posthog/src/PostHogProvider.tsx
+++ tooling/posthog/src/PostHogProvider.tsx
@@ -5,12 +5,12 @@ import { PostHogProvider } from 'posthog-js/react';
 import type { ReactNode } from 'react';
 
 if (typeof window !== 'undefined') {
-	posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
-		api_host: '/ingest',
-		ui_host: 'https://us.posthog.com',
-		person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-	});
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
+    api_host: '/ingest',
+    ui_host: 'https://us.posthog.com',
+    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+  });
 }
 export function CSPostHogProvider({ children }: { children: ReactNode }) {
-	return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }

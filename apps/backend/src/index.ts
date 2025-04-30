d28c3af24fc3a892bfc56303025e9920487eb0ec
Jan Francksen <jan.francksen@gmail.com>
2025-04-30 22:56:09 +0200

    feat: add start of hono

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,25 @@
-import { Hono } from 'hono';
+import { OpenAPIHono } from '@hono/zod-openapi';
+import type { PinoLogger } from 'hono-pino';
+import notFound from './middlewares/not-found';
+import onError from './middlewares/on-error';
+import { logger } from './middlewares/pino-logger';
 
-const app = new Hono();
+type AppBindings = {
+  Variables: {
+    logger: PinoLogger;
+  };
+};
+
+const app = new OpenAPIHono<AppBindings>();
+
+app.use(logger());
 
 app.get('/', (c) => {
   return c.text('Hello Hono!');
 });
+
+app.notFound(notFound);
+
+app.onError(onError);
 
 export default app;
---
+++ apps/backend/src/middlewares/pino-logger.ts
@@ -0,0 +1,10 @@
+import { pinoLogger } from 'hono-pino';
+import pino from 'pino';
+
+import pretty = require('pino-pretty');
+
+export function logger() {
+  return pinoLogger({
+    pino: pino(process.env.NODE_ENV === 'production' ? undefined : pretty()),
+  });
+}

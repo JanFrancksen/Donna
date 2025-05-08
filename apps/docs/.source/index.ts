3c5b9c4ecd2c27011042b10116205a7a13f2bd63
Jan Francksen <jan.francksen@gmail.com>
2025-05-08 13:13:36 +0200

    feat: add docs pages

--- apps/docs/.source/index.ts
+++ apps/docs/.source/index.ts
@@ -1,19 +1,19 @@
 import { _runtime } from 'fumadocs-mdx';
-import * as docs_2 from '../content/(features)/apps/backend.mdx?collection=docs&hash=1746482421095';
-import * as docs_3 from '../content/(features)/apps/desktop.mdx?collection=docs&hash=1746482421095';
-import * as docs_4 from '../content/(features)/apps/docs.mdx?collection=docs&hash=1746482421095';
-import * as docs_5 from '../content/(features)/apps/frontend.mdx?collection=docs&hash=1746482421095';
-import * as docs_6 from '../content/(features)/apps/marketing.mdx?collection=docs&hash=1746482421095';
-import * as docs_7 from '../content/(features)/apps/mobile-app.mdx?collection=docs&hash=1746482421095';
-import * as docs_11 from '../content/(features)/packages/analytics.mdx?collection=docs&hash=1746482421095';
-import * as docs_12 from '../content/(features)/packages/emails.mdx?collection=docs&hash=1746482421095';
-import * as docs_13 from '../content/(features)/packages/tasks.mdx?collection=docs&hash=1746482421095';
-import * as docs_14 from '../content/(features)/packages/ui-library.mdx?collection=docs&hash=1746482421095';
+import * as docs_10 from '../content/(features)/apps/backend.mdx?collection=docs&hash=1746482421095';
+import * as docs_11 from '../content/(features)/apps/desktop.mdx?collection=docs&hash=1746482421095';
+import * as docs_12 from '../content/(features)/apps/docs.mdx?collection=docs&hash=1746482421095';
+import * as docs_13 from '../content/(features)/apps/frontend.mdx?collection=docs&hash=1746482421095';
+import * as docs_14 from '../content/(features)/apps/marketing.mdx?collection=docs&hash=1746482421095';
 // @ts-nocheck -- skip type checking
-import * as docs_15 from '../content/(features)/packages/validators.mdx?collection=docs&hash=1746482421095';
-import * as docs_8 from '../content/(features)/tooling/additional-stuff.mdx?collection=docs&hash=1746482421095';
-import * as docs_9 from '../content/(features)/tooling/github-actions.mdx?collection=docs&hash=1746482421095';
-import * as docs_10 from '../content/(features)/tooling/logger.mdx?collection=docs&hash=1746482421095';
+import * as docs_15 from '../content/(features)/apps/mobile-app.mdx?collection=docs&hash=1746482421095';
+import * as docs_2 from '../content/(features)/architecture/additional-stuff.mdx?collection=docs&hash=1746482421095';
+import * as docs_3 from '../content/(features)/architecture/github-actions.mdx?collection=docs&hash=1746482421095';
+import * as docs_4 from '../content/(features)/architecture/logger.mdx?collection=docs&hash=1746482421095';
+import * as docs_5 from '../content/(features)/tools/analytics.mdx?collection=docs&hash=1746482421095';
+import * as docs_6 from '../content/(features)/tools/emails.mdx?collection=docs&hash=1746482421095';
+import * as docs_7 from '../content/(features)/tools/tasks.mdx?collection=docs&hash=1746482421095';
+import * as docs_8 from '../content/(features)/tools/ui-library.mdx?collection=docs&hash=1746482421095';
+import * as docs_9 from '../content/(features)/tools/validators.mdx?collection=docs&hash=1746482421095';
 import * as docs_0 from '../content/index.mdx?collection=docs&hash=1746482421095';
 import * as docs_1 from '../content/quick-start.mdx?collection=docs&hash=1746482421095';
 import * as _source from '../source.config';
@@ -37,11 +37,75 @@ export const docs = _runtime.docs<typeof _source.docs>(
     },
     {
       info: {
+        path: '(features)/architecture/additional-stuff.mdx',
+        absolutePath:
+          '/Users/jfr/Desktop/code/Donna/apps/docs/content/(features)/architecture/additional-stuff.mdx',
+      },
+      data: docs_2,
+    },
+    {
+      info: {
+        path: '(features)/architecture/github-actions.mdx',
+        absolutePath:
+          '/Users/jfr/Desktop/code/Donna/apps/docs/content/(features)/architecture/github-actions.mdx',
+      },
+      data: docs_3,
+    },
+    {
+      info: {
+        path: '(features)/architecture/logger.mdx',
+        absolutePath:
+          '/Users/jfr/Desktop/code/Donna/apps/docs/content/(features)/architecture/logger.mdx',
+      },
+      data: docs_4,
+    },
+    {
+      info: {
+        path: '(features)/tools/analytics.mdx',
+        absolutePath:
+          '/Users/jfr/Desktop/code/Donna/apps/docs/content/(features)/tools/analytics.mdx',
+      },
+      data: docs_5,
+    },
+    {
+      info: {
+        path: '(features)/tools/emails.mdx',
+        absolutePath:
+          '/Users/jfr/Desktop/code/Donna/apps/docs/content/(features)/tools/emails.mdx',
+      },
+      data: docs_6,
+    },
+    {
+      info: {
+        path: '(features)/tools/tasks.mdx',
+        absolutePath:
+          '/Users/jfr/Desktop/code/Donna/apps/docs/content/(features)/tools/tasks.mdx',
+      },
+      data: docs_7,
+    },
+    {
+      info: {
+        path: '(features)/tools/ui-library.mdx',
+        absolutePath:
+          '/Users/jfr/Desktop/code/Donna/apps/docs/content/(features)/tools/ui-library.mdx',
+      },
+      data: docs_8,
+    },
+    {
+      info: {
+        path: '(features)/tools/validators.mdx',
+        absolutePath:
+          '/Users/jfr/Desktop/code/Donna/apps/docs/content/(features)/tools/validators.mdx',
+      },
+      data: docs_9,
+    },
+    {
+      info: {
         path: '(features)/apps/backend.mdx',
         absolutePath:
           '/Users/jfr/Desktop/code/Donna/apps/docs/content/(features)/apps/backend.mdx',
       },
-      data: docs_2,
+      data: docs_10,
     },
     {
       info: {
@@ -49,7 +113,7 @@ export const docs = _runtime.docs<typeof _source.docs>(
         absolutePath:
           '/Users/jfr/Desktop/code/Donna/apps/docs/content/(features)/apps/desktop.mdx',
       },
-      data: docs_3,
+      data: docs_11,
     },
     {
       info: {
@@ -57,7 +121,7 @@ export const docs = _runtime.docs<typeof _source.docs>(
         absolutePath:
           '/Users/jfr/Desktop/code/Donna/apps/docs/content/(features)/apps/docs.mdx',
       },
-      data: docs_4,
+      data: docs_12,
     },
     {
       info: {
@@ -65,7 +129,7 @@ export const docs = _runtime.docs<typeof _source.docs>(
         absolutePath:
           '/Users/jfr/Desktop/code/Donna/apps/docs/content/(features)/apps/frontend.mdx',
       },
-      data: docs_5,
+      data: docs_13,
     },
     {
       info: {
@@ -73,77 +137,13 @@ export const docs = _runtime.docs<typeof _source.docs>(
         absolutePath:
           '/Users/jfr/Desktop/code/Donna/apps/docs/content/(features)/apps/marketing.mdx',
       },
-      data: docs_6,
+      data: docs_14,
     },
     {
       info: {
         path: '(features)/apps/mobile-app.mdx',
         absolutePath:
           '/Users/jfr/Desktop/code/Donna/apps/docs/content/(features)/apps/mobile-app.mdx',
-      },
-      data: docs_7,
-    },
-    {
-      info: {
-        path: '(features)/tooling/additional-stuff.mdx',
-        absolutePath:
-          '/Users/jfr/Desktop/code/Donna/apps/docs/content/(features)/tooling/additional-stuff.mdx',
-      },
-      data: docs_8,
-    },
-    {
-      info: {
-        path: '(features)/tooling/github-actions.mdx',
-        absolutePath:
-          '/Users/jfr/Desktop/code/Donna/apps/docs/content/(features)/tooling/github-actions.mdx',
-      },
-      data: docs_9,
-    },
-    {
-      info: {
-        path: '(features)/tooling/logger.mdx',
-        absolutePath:
-          '/Users/jfr/Desktop/code/Donna/apps/docs/content/(features)/tooling/logger.mdx',
-      },
-      data: docs_10,
-    },
-    {
-      info: {
-        path: '(features)/packages/analytics.mdx',
-        absolutePath:
-          '/Users/jfr/Desktop/code/Donna/apps/docs/content/(features)/packages/analytics.mdx',
-      },
-      data: docs_11,
-    },
-    {
-      info: {
-        path: '(features)/packages/emails.mdx',
-        absolutePath:
-          '/Users/jfr/Desktop/code/Donna/apps/docs/content/(features)/packages/emails.mdx',
-      },
-      data: docs_12,
-    },
-    {
-      info: {
-        path: '(features)/packages/tasks.mdx',
-        absolutePath:
-          '/Users/jfr/Desktop/code/Donna/apps/docs/content/(features)/packages/tasks.mdx',
-      },
-      data: docs_13,
-    },
-    {
-      info: {
-        path: '(features)/packages/ui-library.mdx',
-        absolutePath:
-          '/Users/jfr/Desktop/code/Donna/apps/docs/content/(features)/packages/ui-library.mdx',
-      },
-      data: docs_14,
-    },
-    {
-      info: {
-        path: '(features)/packages/validators.mdx',
-        absolutePath:
-          '/Users/jfr/Desktop/code/Donna/apps/docs/content/(features)/packages/validators.mdx',
       },
       data: docs_15,
     },
